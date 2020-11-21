b32edcd2bb44cb28b5f462e9c5d13b7d93fbed17
junru-zhu <61392832+junru-zhu@users.noreply.github.com>
2020-11-21 17:03:33 -0500

    Update 1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings.cpp

--- code/backtracking/1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings.cpp
+++ code/backtracking/1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings.cpp
@@ -1,1 +1,25 @@
-fds
+class Solution {
+public:
+    int maxUniqueSplit(string s) {
+        unordered_set<string> cache;
+        split(0, s, cache);
+        return res;
+    }
+private:
+    int res;
+    void split(int cur, string s, unordered_set<string>& cache) {
+        if (cur >= s.length()) {
+            int size = cache.size();
+            res = max(res, size);
+        }
+        
+        for (int i = 1; i <= s.length() - cur; i++) {
+            string sub = s.substr(cur, i);
+            if (!cache.count(sub)) {
+                cache.insert(sub);
+                split(cur + i, s, cache);
+                cache.erase(sub);
+            }
+        }
+    }
+};
